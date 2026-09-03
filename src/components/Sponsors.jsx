2a8601a94afb5bc72ec39564daafb4dba3733694
ezeehere <ezazstudy@gmail.com>
2026-09-03 17:47:25 +0530

    Updating sponsors

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -7,61 +7,30 @@ function Sponsors() {
   return (
     <section
       className="th26-partners"
-      aria-labelledby="th26-partners-title"
+      aria-labelledby="partners-title"
     >
-      <div className="th26-partners-dots" aria-hidden="true" />
-
       <header className="th26-partners-header">
-
-        <h2 id="th26-partners-title">
+        <h2 id="partners-title">
           With support from
         </h2>
 
-        <span aria-hidden="true" />
+        <i aria-hidden="true" />
       </header>
 
-      <div className="th26-partners-board">
-        <article className="th26-partner th26-partner-title">
-          <span
-            className="th26-partner-tape"
-            aria-hidden="true"
-          />
-
-          <p className="th26-partner-label">
-            Title Sponsor
-          </p>
-
-          <img
-            src={oilIndiaLogo}
-            alt="Oil India Limited, Title Sponsor of TEZHACK 2026"
-            className="th26-partner-oil-logo"
-          />
-        </article>
-
-        <div
-          className="th26-partners-divider"
+      <div className="th26-title-sponsor">
+        <span
+          className="th26-paper-tape"
           aria-hidden="true"
-        >
-          <span>×</span>
-        </div>
+        />
 
-        <article className="th26-partner th26-partner-community">
-          <span
-            className="th26-partner-tape"
-            aria-hidden="true"
-          />
+        <p>Official Title Sponsor</p>
 
-          <p className="th26-partner-label">
-            Community Partner
-          </p>
-
-          <img
-            src={gdgJistLogo}
-            alt="Google Developer Group on Campus, Jorhat Institute of Science and Technology"
-            className="th26-partner-gdg-logo"
-          />
-        </article>
+        <img
+          src={oilIndiaLogo}
+          alt="Oil India Limited, Official Title Sponsor"
+        />
       </div>
+
     </section>
   );
 }
